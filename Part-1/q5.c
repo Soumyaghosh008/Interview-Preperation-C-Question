6a3c3418e54b3271d1c810098e8a273853d5f982
Soumyaghosh008 <soumyadeepghosh726@gmail.com>
2025-03-05 14:53:48 +0530

    Pushing the 5th question

---
+++ Part-1/q5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float side, volume;
+
+    printf("Enter the side of the cube: ");
+    scanf("%f", &side);
+
+    volume = side * side * side;
+
+    printf("Volume of the cube: %.2f\n", volume);
+
+    return 0;
+}
