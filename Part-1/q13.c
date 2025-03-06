e9345c111c7e9a2b3c8af3d27bfcf42d8751c52d
Soumyaghosh008 <soumyadeepghosh726@gmail.com>
2025-03-06 21:55:27 +0530

    Pushing the 13th program

---
+++ Part-1/q13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("First 10 numbers of the series: \n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
